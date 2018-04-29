9f9c98e086aa76c6a46398987e5a4908978b977a
mihaelalucia <mihaelalucia28@yahoo.com>
2018-04-29 17:40:42 +0100

    1st upload

---
+++ Gaia.js
@@ -0,0 +1,8 @@
+
+ <script>
+document.getElementById("demo").addEventListener("click", myFunction);
+function myFunction() {
+    document.getElementById("demo").innerHTML = "<h6>Healthy pasta</h6><ul><li>75g young broad beans (use frozen if you can't get fresh)<li>2 x 100g pack asparagus</li><li>170g peas(use frozen if you can't get fresh)</li><li>350g spaghetti or tagliatelle</li><li>1 tbsp olive oil</li><li>1tbsp butter</li><li>200ml tub fromage frais or creme fraiche</li><li>handful fresh chopped herbs (we used mint, parsley and chives)</li><li>parmesan</li>";
+}
+</script>    
+          
